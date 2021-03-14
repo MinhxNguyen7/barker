3a10f777e26f25cbb806fa03429cfaa0c5be48ff
MinhTheMerciless <minhnx.vn@gmail.com>
2021-03-14 18:16:29 +0700

    settings for GCP

---
+++ main.py
@@ -0,0 +1,10 @@
+# Starting point for GCP App Engine
+
+# App Engine by default looks for a main.py file at the root of the app
+# directory with a WSGI-compatible object called app.
+# This file imports the WSGI-compatible object of your Django app,
+# application from mysite/wsgi.py and renames it app so it is discoverable by
+# App Engine without additional configuration.
+
+from twitter_clone_back.wsgi import application
+app = application

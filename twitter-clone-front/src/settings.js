fa474e7dc4796d5ee44b3cb84a573b58e07d16e2
MinhTheMerciless <minhnx.vn@gmail.com>
2021-04-05 00:12:48 +0700

    Add GET_VIEWS_URL

--- twitter-clone-front/src/settings.js
+++ twitter-clone-front/src/settings.js
@@ -1,5 +1,6 @@
 export const API_URL = "/api/"
 export const GET_RANDOM_TWEET_URL = API_URL + "getRandomTweet/"
 export const GET_POSTER_INFO_URL = API_URL + "getUserInfo/"
+export const GET_VIEWERS_URL = API_URL + "viewers/"
 
-export default {API_URL, GET_RANDOM_TWEET_URL, GET_POSTER_INFO_URL}
+export default {API_URL, GET_RANDOM_TWEET_URL, GET_POSTER_INFO_URL, GET_VIEWERS_URL}

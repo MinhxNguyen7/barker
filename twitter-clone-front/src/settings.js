3ed24d128ef2829c46416628ba1fe32d75128f6c
MinhTheMerciless <minhnx.vn@gmail.com>
2021-04-04 23:48:59 +0700

    Created settings.js

---
+++ twitter-clone-front/src/settings.js
@@ -0,0 +1,5 @@
+export const API_URL = "/api/"
+export const GET_RANDOM_TWEET_URL = API_URL + "getRandomTweet/"
+export const GET_POSTER_INFO_URL = API_URL + "getUserInfo/"
+
+export default {API_URL, GET_RANDOM_TWEET_URL, GET_POSTER_INFO_URL}

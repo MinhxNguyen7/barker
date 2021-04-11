4ea5d2f45c93def5b584df96d61ba0ddf1eec3a1
MinhTheMerciless <minhnx.vn@gmail.com>
2021-04-12 00:01:18 +0700

    Added urls for ID2post & viewer2ID

--- twitter-clone-front/src/settings.js
+++ twitter-clone-front/src/settings.js
@@ -1,6 +1,8 @@
 export const API_URL = "/api/"
 export const GET_RANDOM_TWEET_URL = API_URL + "getRandomTweet/"
+export const GET_POST_FROM_ID = API_URL + "getPostFromID/"
 export const GET_POSTER_INFO_URL = API_URL + "getUserInfo/"
 export const GET_VIEWERS_URL = API_URL + "viewers/"
+export const GET_IDs_FROM_VIEWER_URL = API_URL + "ViewerToId/"
 
-export default {API_URL, GET_RANDOM_TWEET_URL, GET_POSTER_INFO_URL, GET_VIEWERS_URL}
+export default {API_URL, GET_RANDOM_TWEET_URL, GET_POST_FROM_ID ,GET_POSTER_INFO_URL, GET_VIEWERS_URL, GET_IDs_FROM_VIEWER_URL}

224c104cc0f3fb6e914c8843a6c088d480ad406b
MinhTheMerciless <minhnx.vn@gmail.com>
2021-04-12 18:09:35 +0700

    Added script for compilation and deployment

---
+++ collect&deploy.bat
@@ -0,0 +1,19 @@
+ECHO OFF
+
+:: Activate environment
+call d:/Apps/Anaconda/Scripts/activate mysite
+
+d:
+cd Programming\twitter_clone\twitter-clone-back
+
+:: Build static React site
+npm run build
+
+:: Collect static files into Django
+cd ..
+python manage.py collectstatic
+
+:: Deploy!
+gcloud app deploy
+
+cmd /k

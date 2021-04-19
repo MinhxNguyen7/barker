ebda04c14d5cd9d70620e26d5046dfb3331c751b
MinhTheMerciless <minhnx.vn@gmail.com>
2021-04-19 22:59:08 +0700

    Update settings.js

--- twitter-clone-front/src/settings.js
+++ twitter-clone-front/src/settings.js
@@ -1,8 +1,12 @@
 export const API_URL = "/api/"
+export const GET_WHOLE_TWEET_URL = API_URL + "wholePost/"
 export const GET_RANDOM_TWEET_URL = API_URL + "getRandomTweet/"
 export const GET_POST_FROM_ID = API_URL + "getPostFromID/"
 export const GET_POSTER_INFO_URL = API_URL + "getUserInfo/"
 export const GET_VIEWERS_URL = API_URL + "viewers/"
 export const GET_IDs_FROM_VIEWER_URL = API_URL + "ViewerToId/"
+export const GET_EXPLANATION_URL = API_URL + "getExplanation/"
 
-export default {API_URL, GET_RANDOM_TWEET_URL, GET_POST_FROM_ID ,GET_POSTER_INFO_URL, GET_VIEWERS_URL, GET_IDs_FROM_VIEWER_URL}
+export default {
+    API_URL, GET_WHOLE_TWEET_URL, GET_RANDOM_TWEET_URL, GET_POST_FROM_ID ,GET_POSTER_INFO_URL, GET_VIEWERS_URL, GET_IDs_FROM_VIEWER_URL, GET_EXPLANATION_URL
+}

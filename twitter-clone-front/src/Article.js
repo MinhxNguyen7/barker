309de4627ced74642a3bc6307d1bba1a508a57f3
MinhTheMerciless <minhnx.vn@gmail.com>
2021-05-08 23:12:47 +0700

    Infractructure for article retrieval and display

--- twitter-clone-front/src/Article.js
+++ twitter-clone-front/src/Article.js
@@ -1,20 +1,34 @@
 import React from "react";
+import settings from "./settings"
+import axios from "axios";
 
 class Article extends React.Component{
     constructor(props){
         super(props)
         this.state={text:"", title:"", image_url:""}
     }
+
+    getArticle(){
+        console.log("Getting article " + this.props.id)
+        axios
+            .get(settings.GET_ARTICLE_FROM_ID_URL + this.props.id + "/")
+            .then((response)=>{
+                const d = response.data
+                this.setState({title:d.title, text:d.text})
+                this.forceUpdate()
+            })
+            .catch(err=>{console.log(err)})
+    }
     
     componentDidMount(){
-        
+        this.getArticle()
     }
 
     render(){
         console.log("Article with id: " + this.props.id)
         return(
             <div className="Article">
-                This page is a work in progress
+                {this.state.text}
             </div>
         )
     }

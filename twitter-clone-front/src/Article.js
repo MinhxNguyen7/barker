9d4c15a7ac944db7d81e9a1b666807d228643cbc
MinhTheMerciless <minhnx.vn@gmail.com>
2021-05-09 21:07:18 +0700

    Cleaning

--- twitter-clone-front/src/Article.js
+++ twitter-clone-front/src/Article.js
@@ -30,14 +30,12 @@ class Article extends React.Component{
         console.log("Article with id: " + this.props.id)
         return(
             <html class="wf-montserrat-n4-active wf-sourcesanspro-n2-active wf-active">
-                <head>
-                    <meta charset="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-                    <script>var __adobewebfontsappname__="dreamweaver"</script>
-                    <link rel="stylesheet" href="https://use.edgefonts.net/c/e2c03d/1w;montserrat,2,2cm5Ph:W:n4;source-sans-pro,2,2cm9PC:W:n2/l" media="all"/>
-                    <script src="http://use.edgefonts.net/montserrat:n4:default;source-sans-pro:n2:default.js" type="text/javascript"></script>
-                </head>
+                <script>var __adobewebfontsappname__="dreamweaver"</script>
+                <link rel="stylesheet" href="https://use.edgefonts.net/c/e2c03d/1w;montserrat,2,2cm5Ph:W:n4;source-sans-pro,2,2cm9PC:W:n2/l" media="all"/>
+                <script src="http://use.edgefonts.net/montserrat:n4:default;source-sans-pro:n2:default.js" type="text/javascript"/>
 
                 <body>
                     <header>

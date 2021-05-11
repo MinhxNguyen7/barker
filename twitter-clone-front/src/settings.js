d419695e79c5b2938c1dc36d5ec7ddf4d5f0eba3
MinhTheMerciless <minhnx.vn@gmail.com>
2021-05-11 14:05:17 +0700

    Fixed setting for API article request

--- twitter-clone-front/src/settings.js
+++ twitter-clone-front/src/settings.js
@@ -3,7 +3,7 @@ export const GET_WHOLE_TWEET_URL = API_URL + "wholePost/"
 export const GET_POST_FROM_ID = API_URL + "getPostFromID/"
 export const GET_VIEWERS_URL = API_URL + "viewers/"
 export const GET_IDs_FROM_VIEWER_URL = API_URL + "ViewerToId/"
-export const GET_ARTICLE_FROM_ID_URL = API_URL + "/article/"
+export const GET_ARTICLE_FROM_ID_URL = API_URL + "article/"
 
 export default {
     API_URL, GET_WHOLE_TWEET_URL, GET_POST_FROM_ID, GET_VIEWERS_URL, GET_IDs_FROM_VIEWER_URL, GET_ARTICLE_FROM_ID_URL

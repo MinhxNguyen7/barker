8002111647456d14973c1fc913c7d49203e50f2c
MinhTheMerciless <minhnx.vn@gmail.com>
2021-05-19 17:28:26 +0700

    Random rotating secret_key

--- twitter_clone_back/settings.py
+++ twitter_clone_back/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os.path
+from random import choice
+from string import printable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&qs21bjjw$m6lou=7f54@e&dcs2ii5kh-b6o+=k-x1+ko1(@#w'
+SECRET_KEY = ''.join(choice(printable) for i in range(50))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

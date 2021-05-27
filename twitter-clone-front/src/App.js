43b6c1b058634f2b6d111429b430e3356a232ea1
MinhTheMerciless <minhnx.vn@gmail.com>
2021-05-27 22:11:10 +0700

    Revert "Minor tweaks"
    This reverts commit e569dd9114455b8056a9b821255d7d31f10b3d60.

--- twitter-clone-front/src/App.js
+++ twitter-clone-front/src/App.js
@@ -18,13 +18,14 @@ export default class App extends React.Component {
   }
 
   componentDidMount(){
-    if(window.location.pathname === "/"){ // Only add new posts on main screen
-      axios
+    const pathname = window.location.pathname
+    if(pathname === "/"){ // Only add new posts on main screen
+      axios // Get List of viewers
         .get(settings.GET_VIEWERS_URL)
         .then(response =>{
-          this.setState({viewersList: response.data}, 
+          this.setState({viewersList: response.data.sort()}, // Sorts viewers in alphabetical order
             ()=>this.setState({loaded: true}, 
-              ()=>{this.forceUpdate();console.log("Recieved users: " + this.state.viewersList)}
+              ()=>console.log("Recieved users: " + this.state.viewersList)
             )
           )
         })
@@ -53,12 +54,13 @@ export default class App extends React.Component {
 
   render(){
     const pathname = window.location.pathname
+    const viewer = String(this.state.viewersList[this.state.viewerNum])
 
     let body = null
     if(pathname === "/"){
       const viewersObj = {
         list: this.state.viewersList, num: this.state.viewerNum, setNum: this.setViewer}
-      const viewer = String(this.state.viewersList[this.state.viewerNum])
+      
       body = (
               <div className="app">
                 <Sidebar switchClick={this.switchClick}/>
